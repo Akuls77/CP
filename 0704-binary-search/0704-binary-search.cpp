c6267dcbf125f3001425dada5cbe18d3179917d1
Akuls77 <148907553+Akuls77@users.noreply.github.com>
2025-07-19 16:20:51 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.48%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int binarys(vector<int> &nums, int low, int high, int target){
+        if(low>high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(target == nums[mid]){
+            return mid;
+        }
+        else if(target < nums[mid]){
+            return binarys(nums, low, mid-1, target);
+        }
+        else{
+            return binarys(nums, mid+1, high, target);
+        }
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarys(nums, 0, nums.size()-1, target);
+    }
+};
