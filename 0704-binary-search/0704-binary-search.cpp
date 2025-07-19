a3bea5c772fd84a72e91a158b770eb4a3e061403
Akuls77 <148907553+Akuls77@users.noreply.github.com>
2025-07-19 16:25:52 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.48%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int binarys(vector<int> &nums, int low, int high, int target){
-        if(low>high){
-            return -1;
-        }
-        int mid = (low+high)/2;
-        if(target == nums[mid]){
-            return mid;
-        }
-        else if(target < nums[mid]){
-            return binarys(nums, low, mid-1, target);
-        }
-        else{
-            return binarys(nums, mid+1, high, target);
-        }
-    }
+    // int binarys(vector<int> &nums, int low, int high, int target){
+    //     if(low>high){
+    //         return -1;
+    //     }
+    //     int mid = (low+high)/2;
+    //     if(target == nums[mid]){
+    //         return mid;
+    //     }
+    //     else if(target < nums[mid]){
+    //         return binarys(nums, low, mid-1, target);
+    //     }
+    //     else{
+    //         return binarys(nums, mid+1, high, target);
+    //     }
+    // }
 
     int search(vector<int>& nums, int target) {
-        return binarys(nums, 0, nums.size()-1, target);
+        //return binarys(nums, 0, nums.size()-1, target);
+
+        int n= nums.size();
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 };
