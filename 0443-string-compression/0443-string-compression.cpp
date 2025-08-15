0698f2ab3c57cc212df71b888c818cd29fa3fa4d
Akuls77 <148907553+Akuls77@users.noreply.github.com>
2025-08-15 12:36:12 +0530

    Time: 0 ms (100%), Space: 13.7 MB (75.18%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -6,14 +6,15 @@ public:
         for(int i=0; i<n; i++){
             char ch = chars[i];
             int cnt=0;
+            chars[idx]= ch;
+            idx++;
+
             while(i<n && chars[i]==ch){
                 i++;
                 cnt++;
             }
 
-            chars[idx]= ch;
-            idx++;
-            if(cnt>1){
+            if(cnt>1){     //to store cnt in array
                 string str = to_string(cnt);
                 for(char digit: str){
                     chars[idx] = digit;
@@ -22,6 +23,7 @@ public:
             }
             i--;    //when inner while loop finishes, idx is pointing to at first char of next group and outer for loop again does i++ so to balance it we do i--
         }
+        chars.resize(idx);
         return idx;
-    }  
+    }
 };
