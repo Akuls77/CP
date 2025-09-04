38226e91026f8b21dc2002d4999ff5e4da8e372b
Akuls77 <148907553+Akuls77@users.noreply.github.com>
2025-09-04 11:28:16 +0530

    Time: 3 ms (52.10%) | Memory: 9.8 MB (48.42%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        
+        if(m!=n){
+            return false;
+        }
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        // return s==t;
+
+        int freq[26] = {0};
+        for(int i=0; i<n; i++){
+            freq[s[i] - 'a']++;
+        }
+        for(int i=0; i<n; i++){
+            freq[t[i] - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
